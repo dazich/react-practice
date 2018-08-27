b9d9b7871af6b69f712f6b7549b2fbd715021a63
dazi <497688262@qq.com>
2018-08-27 22:50:21 +0800

    update

--- readme.md
+++ readme.md
@@ -3,6 +3,7 @@
 
 1. 基本webpack4环境搭建  react HMR等 √
 2. react-router4 redux √
+2.5 redux与request的处理（soga？）
 3. ssr
 4. docker
 
--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { Router, Route, Switch } from "react-router-dom";
 import { hot } from 'react-hot-loader'
-import Counter from './Counter'
+import routes from './routes'
+import history from './utils/history'
 
-const App = () => (
-    <h1>
-        Hello, world.<br />
-        <Counter />
-    </h1>
-)
+// wrap <Route> and use this everywhere instead, then when
+// sub routes are added to any route it'll work
+const RouteWithSubRoutes = route => (
+    <Route
+        path={route.path}
+        render={props => (
+            // pass the sub-routes down to keep nesting
+            <route.component {...props} routes={route.routes} />
+        )}
+    />
+);
 
-export default hot(module)(App)
+class App extends Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Router history={history}>
+                <Switch>
+                    {routes.map((route, i) => <RouteWithSubRoutes key={i} {...route} />)}
+                </Switch>
+            </Router>
+        )
+    }
+}
+
+export default hot(module)(App);
